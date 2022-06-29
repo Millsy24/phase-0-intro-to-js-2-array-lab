ce6209bb5afad6024f842380d3a5bfbb45a37a51
Lukas Mills <lukascodes64@gmail.com>
2022-06-29 12:06:33 -0400

    commit tree 26ad4fb33f2822a8938d6c34fb940d1ee350e842

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+// Write your solution here
+const cats = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+   
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    let newArray = cats.slice()
+    newArray.push(name)
+    return newArray
+
+}
+function prependCat(name){
+    let newArray = cats.slice()
+    newArray.unshift(name)
+    return newArray
+}
+
+function removeLastCat(){
+    let newArray = cats.pop()
+    newArray.unshift()
+    return newArray
+}
